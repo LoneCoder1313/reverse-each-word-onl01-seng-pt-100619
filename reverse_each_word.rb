77da6eb1a0e67f1d28270864a7d59212a7fae31a
Ricky dildy <rdildy92@gmail.com>
2019-10-26 06:06:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ sentence1 = "Hello thre, and how are you?"
 
 def reverse_each_word(sentence1) do
   reverse_each_word.gets reversed |sentence1|
-  puts ""
+  puts "olleh, ereht dna woh era ?uoy"
   
